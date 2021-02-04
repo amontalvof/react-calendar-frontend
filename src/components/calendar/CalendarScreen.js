5cb589451b542ebe571bc8bce7c461f5428f71a0
amontalvof <amontalvof92@gmail.com>
2021-02-04 13:12:24 -0600

    feat: customized box and trigger calendar events

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -3,6 +3,8 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Navbar } from '../ui/Navbar';
+import { CalendarEvent } from './CalendarEvent';
+import { useState } from 'react';
 
 // import { messages as espanolMessages } from '../helpers/calendar-messages-es';
 // import 'moment/locale/es'; // to put the dates of the moment in Spanish
@@ -12,17 +14,38 @@ const localizer = momentLocalizer(moment);
 
 const events = [
     {
-        title: 'Cumpleaños de Andy',
+        title: 'Cumpleaños del jefe',
         start: moment().toDate(),
         end: moment().add(1, 'hours').toDate(),
         bgcolor: '#fafafa',
         notes: 'comprar el pastel',
+        user: {
+            _id: '123',
+            name: 'Andy',
+        },
     },
 ];
 
 export const CalendarScreen = () => {
+    const [lastView, setLastView] = useState(
+        localStorage.getItem('lastView') || 'month'
+    );
+
+    const onDoubleClick = (event) => {
+        console.log('onDoubleClick', event);
+    };
+
+    const onSelect = (event) => {
+        console.log('onSelect', event);
+    };
+
+    const onViewChange = (event) => {
+        setLastView(event);
+        localStorage.setItem('lastView', event);
+    };
+
     const eventStyleGetter = (event, start, end, isSelected) => {
-        console.log(event, start, end, isSelected);
+        // console.log(event, start, end, isSelected);
 
         const style = {
             backgroundColor: '#367cf7',
@@ -45,6 +68,11 @@ export const CalendarScreen = () => {
                 startAccessor="start"
                 endAccessor="end"
                 eventPropGetter={eventStyleGetter}
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelect}
+                onView={onViewChange}
+                view={lastView}
+                components={{ event: CalendarEvent }}
                 // messages={espanolMessages}
             />
         </div>
