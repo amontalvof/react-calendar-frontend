3fdbae8a81288caf61fe4b8d4912e8236866500a
amontalvof <amontalvof92@gmail.com>
2021-02-18 14:05:01 -0600

    feat: auth reducer

--- src/types/types.js
+++ src/types/types.js
@@ -7,4 +7,12 @@ export const types = {
     eventClearActiveEvent: '[event] Clear active event',
     eventUpdated: '[event] updated',
     eventDeleted: '[event] deleted',
+
+    authChecking: '[auth] Checking login state',
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
 };
