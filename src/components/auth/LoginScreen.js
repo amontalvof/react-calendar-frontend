c99081a19fdb4d8a65ffe191b5a05393fc2f5717
amontalvof <amontalvof92@gmail.com>
2021-02-19 14:45:34 -0600

    fix: fixed styles in login screen

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -42,100 +42,101 @@ export const LoginScreen = () => {
     };
 
     return (
-        <div className="container login-container">
-            <div className="row">
-                <div className="col-md-6 login-form-1">
-                    <h3>Log in</h3>
-                    <form onSubmit={handleLogin}>
-                        <div className="form-group">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Email"
-                                name="loginEmail"
-                                value={loginEmail}
-                                onChange={handleLoginInputChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Password"
-                                name="loginPassword"
-                                value={loginPassword}
-                                onChange={handleLoginInputChange}
-                            />
-                            <p className="helpTextLogin">
-                                The password must be at least 6 characters long.
-                            </p>
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="submit"
-                                className="btnSubmit"
-                                value="Login"
-                            />
-                        </div>
-                    </form>
-                </div>
-
-                <div className="col-md-6 login-form-2">
-                    <h3>Sign in</h3>
-                    <form onSubmit={handleRegister}>
-                        <div className="form-group">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Name"
-                                name="registerName"
-                                value={registerName}
-                                onChange={handleRegisterInputChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="email"
-                                className="form-control"
-                                placeholder="Email"
-                                name="registerEmail"
-                                value={registerEmail}
-                                onChange={handleRegisterInputChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Password"
-                                name="registerPassword1"
-                                value={registerPassword1}
-                                onChange={handleRegisterInputChange}
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Repeat password"
-                                name="registerPassword2"
-                                value={registerPassword2}
-                                onChange={handleRegisterInputChange}
-                            />
-                            <p className="helpTextRgister">
-                                The password must be at least 6 characters long.
-                            </p>
-                        </div>
-
-                        <div className="form-group">
-                            <input
-                                type="submit"
-                                className="btnSubmit"
-                                value="Create account"
-                            />
-                        </div>
-                    </form>
+        <div className="login-container">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 login-form-1">
+                        <h3>Log in</h3>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Email"
+                                    name="loginEmail"
+                                    value={loginEmail}
+                                    onChange={handleLoginInputChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Password"
+                                    name="loginPassword"
+                                    value={loginPassword}
+                                    onChange={handleLoginInputChange}
+                                />
+                                <p className="helpTextLogin">
+                                    The password must be at least 6 characters
+                                    long.
+                                </p>
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="submit"
+                                    className="btnSubmit"
+                                    value="Login"
+                                />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="col-md-6 login-form-2">
+                        <h3>Sign in</h3>
+                        <form onSubmit={handleRegister}>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Name"
+                                    name="registerName"
+                                    value={registerName}
+                                    onChange={handleRegisterInputChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    placeholder="Email"
+                                    name="registerEmail"
+                                    value={registerEmail}
+                                    onChange={handleRegisterInputChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Password"
+                                    name="registerPassword1"
+                                    value={registerPassword1}
+                                    onChange={handleRegisterInputChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Repeat password"
+                                    name="registerPassword2"
+                                    value={registerPassword2}
+                                    onChange={handleRegisterInputChange}
+                                />
+                                <p className="helpTextRgister">
+                                    The password must be at least 6 characters
+                                    long.
+                                </p>
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="submit"
+                                    className="btnSubmit"
+                                    value="Create account"
+                                />
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
