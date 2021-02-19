5d93cdbf88d65d11a34a8440fb2747ee152d024f
amontalvof <amontalvof92@gmail.com>
2021-02-18 20:45:06 -0600

    feat: maintain authentication status

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,14 @@
 import Swal from 'sweetalert2';
-import { fetchSinToken } from '../helpers/fetch';
+import { fetchWithoutToken, fetchWithToken } from '../helpers/fetch';
 import { types } from '../types/types';
 
 export const startLogin = (email, password) => {
     return async (dispatch) => {
-        const resp = await fetchSinToken('auth', { email, password }, 'POST');
+        const resp = await fetchWithoutToken(
+            'auth',
+            { email, password },
+            'POST'
+        );
         const body = await resp.json();
         if (body.ok) {
             localStorage.setItem('token', body.token);
@@ -23,7 +27,7 @@ export const startLogin = (email, password) => {
 
 export const startRegister = (email, password, name) => {
     return async (dispatch) => {
-        const resp = await fetchSinToken(
+        const resp = await fetchWithoutToken(
             'auth/new',
             { email, password, name },
             'POST'
@@ -43,6 +47,31 @@ export const startRegister = (email, password, name) => {
         }
     };
 };
+
+export const startChecking = () => {
+    return async (dispatch) => {
+        const resp = await fetchWithToken('auth/renew');
+        const body = await resp.json();
+        console.log(body);
+        if (body.ok) {
+            localStorage.setItem('token', body.token);
+            localStorage.setItem('token-init-time', new Date().getTime());
+            dispatch(
+                login({
+                    uid: body.uid,
+                    name: body.name,
+                })
+            );
+        } else {
+            Swal.fire('Error!', body.message, 'error');
+            dispatch(checkingFinish());
+        }
+    };
+};
+
+const checkingFinish = () => ({
+    type: types.authCheckingFinish,
+});
 
 const login = (user) => ({
     type: types.authLogin,
